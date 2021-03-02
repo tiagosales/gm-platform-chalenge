cc5ac9d95bb39743cbc851c7be3760901315461c
Tiago Sales <tiagosales.ti@gmail.com>
2021-03-02 22:38:46 +0000

    Another way to get repo uri

--- get-ecr-repo.sh
+++ get-ecr-repo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 REPO_NAME=$1
 REGION=$2
-aws ecr --no-verify-ssl describe-repositories --repository-names ${REPO_NAME} --region ${REGION} | grep repositoryUri | awk -F: {'print $2'} | tr -d ",|awk {'print $1'}
+aws ecr --no-verify-ssl describe-repositories --repository-names ${REPO_NAME} --region ${REGION} | grep repositoryUri | awk -F: {'print $2'} | tr -d \",|awk {'print $1'}
